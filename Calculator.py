bcfd721136072bc83cebe739be198d82fce29430
MrSe7en <44005520+nerioa7@users.noreply.github.com>
2021-01-05 03:03:30 +0000

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,12 +1,16 @@
+//defining the add for x,y, which returns the value x,y
 def add(x,y):
     return x + y
-    
+
+//defining the subtract for x,y, which returns the value x,y
 def subtract (x, y):
     return x - y
-    
+
+//defining the divide for x,y, which returns the value x,y
 def divide(x,y):
     return x / y
-    
+
+//defining the multiply for x,y, which returns the value x,y
 def multiply (x,y):
     return x * y
 print (".................Pthon Calculator.................      ")
@@ -16,14 +20,18 @@ print ("2-> Minus")
 print ("3-> Divide")
 print ("4-> Multiplication")
 
+//while loop only execute if the condition is true
 while True:
-    
+
+    //here is the option for the user to select the choosing of operator 
     option = input("Enter a pick (1,2,3,4) \n")
     
     if option in ("1", "2" , "3", "4" ):
+      //user input of their own choice
         num1 = int(input("Enter a Number"))
         num2 = int(input("Enter a Number"))
         
+        // if condition 
     if option == "1":
         print (num1, "+" ,num2, "=", add(num1, num2))
         
